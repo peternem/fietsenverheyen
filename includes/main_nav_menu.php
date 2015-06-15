22f9a0a25bf5a986a501f5270e260328f137423b
peternem <mjpeternell@gmail.com>
2015-06-14 21:15:16 -0700

    Hours fix. PHP main nav fix. minor css fix main nav.

--- includes/main_nav_menu.php
+++ includes/main_nav_menu.php
@@ -3,11 +3,11 @@
 </div>
 <nav>
 	<ul>
-		<li class="toggle-hide <? if ($pageName=="aboutPage") echo "activePage"; ?>"><a href="about.php" title="About">ABOUT</a></li>
-		<li class="toggle-hide <? if ($pageName=="brandsPage") echo "activePage"; ?>"><a href="brands.php" title="Brands">BRANDS</a></li>
-		<li class="toggle-hide <? if ($pageName=="handBuiltPage") echo "activePage"; ?>"><a href="hand-built.php" title="Hand Built">HAND BUILT</a></li>
-		<li class="toggle-hide <? if ($pageName=="contactPage") echo "activePage"; ?>"><a href="contact.php" title="Contact">CONTACT</a></li>
-		<li class="toggle-hide <? if ($pageName=="locationPage") echo "activePage"; ?>"><a href="location.php" title="Location">LOCATION</a></li>
+		<li class="toggle-hide <?php if ($pageName=="aboutPage") echo "activePage"; ?>"><a href="about.php" title="About">ABOUT</a></li>
+		<li class="toggle-hide <?php if ($pageName=="brandsPage") echo "activePage"; ?>"><a href="brands.php" title="Brands">BRANDS</a></li>
+		<li class="toggle-hide <?php if ($pageName=="handBuiltPage") echo "activePage"; ?>"><a href="hand-built.php" title="Hand Built">HAND BUILT</a></li>
+		<li class="toggle-hide <?php if ($pageName=="contactPage") echo "activePage"; ?>"><a href="contact.php" title="Contact">CONTACT</a></li>
+		<li class="toggle-hide <?php if ($pageName=="locationPage") echo "activePage"; ?>"><a href="location.php" title="Location">LOCATION</a></li>
 		<li class="toggle" title="Menu"><span class="toggle-menu-bar"></span><span class="toggle-menu-bar"></span><span class="toggle-menu-bar"></span></li>
 	</ul>
 
--- location.php
+++ location.php
@@ -50,11 +50,11 @@
 
 						<h2>Store Hours</h2>
 						<ul>
-							<li>Monday - 10:00-12:00, 13:00-18:00</li>
+							<li>Monday - 13:00-18:00</li>
 							<li>Tuesday - closed</li>
 							<li>Wednesday - 10:00-12:00, 13:00-18:00</li>
 							<li>Thursday - 13:00 - 20:00</li>
-							<li>Friday - 10:00-12:00, 13:00-18:00</li>
+							<li>Friday - 13:00-18:00</li>
 							<li>Saturday - 10:00-17:00</li>
 							<li>Sunday - Closed</li>
 						</ul>
